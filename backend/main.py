9620aeb289f906c0faed67b747c5d323d6594293
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-29 18:24:20 +0700

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from core.hardware_scanner import scan_hardware
+from core.cpu_patch import patch_cpu
+from core.gpu_patch import patch_gpu
+from core.config_generator import generate_config
+from core.efi_builder import build_efi
+
+def main():
+
+    print("🚀 Hackintosh AI Builder")
+
+    hardware = scan_hardware()
+
+    cpu_patch = patch_cpu(hardware)
+    gpu_patch = patch_gpu(hardware)
+
+    config = generate_config(
+        hardware,
+        cpu_patch,
+        gpu_patch
+    )
+
+    build_efi(config)
+
+    print("✅ EFI CREATED")
+
+if __name__ == "__main__":
+    main()
